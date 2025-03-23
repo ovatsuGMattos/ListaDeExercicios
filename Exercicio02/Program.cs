87bc8aaa137ca28488de143b0c8a5216a7739e34
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-03-23 16:51:55 -0300

    Implementado exercicio 03

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+
+        //Crie um programa para calcular o volume de um Cilindro
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio do cilindro:");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura do cilindro:");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double volume = Math.PI* Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindro é: {volume} cm³.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        //Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma viagem. O programa deve solicitar ao usuário:
+        //● A quilometragem inicial do veículo no início da viagem.
+        //● A quilometragem final ao término da viagem.
+        //● A quantidade de combustível consumida durante a viagem(em litros).
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a quilometragem inicial:");
+            double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a quilometragem final:");
+            double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o consumo de combustivel após a viagem (litros): ");
+            double consumoCombustivel = Convert.ToDouble(Console.ReadLine());
+
+            double distanciaPercorrida = kmFinal - kmInicial;
+
+            double consumoPorKm = distanciaPercorrida / consumoCombustivel;
+
+            Console.WriteLine($"O consumo de combustivel durante o percurso foi de ~ {consumoPorKm} km/l");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
