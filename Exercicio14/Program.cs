40512770828dcfaf3233b6ad7615cda0c459716b
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-03-23 21:29:30 -0300

    implementado exercicio 15

--- Exercicio14/Program.cs
+++ Exercicio14/Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
+        //Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Classificação em Ordem Decrescente");
+            Console.WriteLine("--------------------------------------");
+
+            Console.Write("Digite o primeiro valor: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o terceiro valor: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                if (b > c)
+                    Console.WriteLine($"{a}, {b}, {c}");
+                else
+                    Console.WriteLine($"{a}, {c}, {b}");
+            }
+            else if (b > a && b > c)
+            {
+                if (a > c)
+                    Console.WriteLine($"{b}, {a}, {c}");
+                else
+                    Console.WriteLine($"{b}, {c}, {a}");
+            }
+            else
+            {
+                if (a > b)
+                    Console.WriteLine($"{c}, {a}, {b}");
+                else
+                    Console.WriteLine($"{c}, {b}, {a}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Exercicio15/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio15
+{
+    internal class Program
+    {
+        //Elabore um algoritmo que leia o peso e a altura de um adulto e mostre sua condição de acordo com a listagem abaixo:● IMC em adultos Condição
+        //Abaixo de 18,5
+        //Abaixo do peso
+        //Entre 18,5 e 25 Peso normal
+        //Entre 25 e 30
+        //Acima do peso
+        //Acima de 30 obeso
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
