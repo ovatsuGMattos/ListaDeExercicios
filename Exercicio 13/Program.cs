ab4519c6cbfd8dbdee20a857d9a46e818e7c35a9
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-03-23 21:12:09 -0300

    Adicionado exercicio 13

---
+++ Exercicio 13/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio13
+{
+    internal class Program
+    {
+        //Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("=== Verificação de Soma ===");
+            Console.WriteLine("---------------------------");
+
+            Console.Write("Digite o valor de A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            if (A + B < C)
+            {
+                Console.WriteLine("\nA soma de A + B é menor que C.");
+            }
+            else
+            {
+                Console.WriteLine("\nA soma de A + B NÃO é menor que C.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio12/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio12
+{
+    internal class Program
+    {
+        //Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%. Após o aumento, desconte 8% de impostos.
+        //Imprima o salário inicial, o salário com o aumento e o salário final.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("=== Calculo de Aumento e Impostos ===");
+            Console.WriteLine("-------------------------------------");
+
+            Console.Write("Digite o salário do funcionário: R$ ");
+            double salarioInicial = Convert.ToDouble(Console.ReadLine());
+
+            double salarioComAumento = salarioInicial * 1.15;
+
+            double salarioFinal = salarioComAumento * 0.92;
+
+            Console.WriteLine($"\nSalário inicial: R$ {salarioInicial:F2}");
+            Console.WriteLine($"Salário com aumento de 15%: R$ {salarioComAumento:F2}");
+            Console.WriteLine($"Salário final após desconto de 8% de impostos: R$ {salarioFinal:F2}");
+
+            Console.ReadLine();
+        }
+    }    
+}
